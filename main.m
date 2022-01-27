828be93eb31b56563395f975a1cf6b9beb10cec3
arminpan <arminpp1379@gmail.com>
2022-01-27 23:46:53 +0330

    main file

---
+++ main.m
@@ -0,0 +1,9 @@
+%% Armin Panjehpour - Ardavan Kaviany - Jan,Feb 2021
+%% Foundations of Neuroscience Project
+% Processings on electrophysiology data from Motor Cortex of 
+% Macaque monkey
+% Task : Reach to Grasp
+%% Dataset Information
+% Data Acquisition using a 10*10 electrode array 
+%% Part 0 - Load Dataset, Initialization
+
